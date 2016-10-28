ce4342c3479720a2af15622adbb01c8a2cd5d0b2
assafmo <assaf.morami@gmail.com>
2016-10-28 19:49:54 +0300

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Only NBA tweets from [ESPN Stats & Info](https://twitter.com/ESPNStatsInfo)
+
+## License
+The source code is available under the MIT [License](./LICENSE.md).
---
+++ build_html.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo '<html><head><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></head><body><center>' > index.html
+
+newest_id_to_get=$(\
+	curl -s --compressed 'https://twitter.com/ESPNStatsInfo' | \
+	tr -d '\n' | \
+	sed 's/data-tweet-id/\ndata-tweet-id/g' | \
+	grep -F TweetText | \
+	head -1 | \
+	awk -F '"' '{print $2}' \
+)
+
+
+#get last x tweets
+for ((i=1;i<=10;i++)); do 
+	tweets=$( \
+		curl -s --compressed "https://mobile.twitter.com/i/rw/profile/timeline?max_id=$newest_id_to_get&screen_name=ESPNStatsInfo&type=tweets" | \
+		tr -d '\n' | \
+		sed 's/data-tweet-id/\ndata-tweet-id/g' | \
+		grep -F TweetText \
+	)
+
+	found_ids=$( \
+		echo "$tweets" | \
+		grep -F -f keywords.list | \
+		awk -F '"' '{print $2}' \
+	)
+
+	if [ ! -z "$found_ids" ]; then
+		echo "$found_ids" | \
+		xargs -n 1 -I {} curl -s --compressed "https://api.twitter.com/1.1/statuses/oembed.json?id={}" | \
+		jq --raw-output '.html' | \
+		sed 's/<script.*script>/ /g' >> index.html
+	fi
+
+	newest_id_to_get=$(echo "$tweets" | tail -1 | awk -F '"' '{print $2}')
+done
+
+echo '</center></body></<html>' >> index.html
