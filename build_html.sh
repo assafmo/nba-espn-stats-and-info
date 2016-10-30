cbd6c422d6bb10731bacff904669dab10b19a96a
assafmo <assaf.morami@gmail.com>
2016-10-30 21:40:10 +0200

    fix players names, loop until 25 tweets

--- build_html.sh
+++ build_html.sh
@@ -18,9 +18,11 @@ newest_id_to_get=$(\
 )
 
 
-#get last x pages of tweets
-x=10
-for ((i=1;i<=x;i++)); do 
+#get last max tweets
+max=25
+count=0
+pages=0
+while [ $count -lt $max ]; do 
 	tweets=$( \
 		curl -s --compressed "https://mobile.twitter.com/i/rw/profile/timeline?max_id=$newest_id_to_get&screen_name=ESPNStatsInfo&type=tweets" | \
 		tr -d '\n' | \
@@ -39,11 +41,15 @@ for ((i=1;i<=x;i++)); do
 		xargs -n 1 -I {} curl -s --compressed "https://api.twitter.com/1.1/statuses/oembed.json?id={}" | \
 		jq --raw-output '.html' | \
 		sed 's/<script.*script>/ /g' >> index.html
+		
+		count=$((count+$(echo "$found_ids" | wc -l)))
 	fi
 
 	newest_id_to_get=$(echo "$tweets" | tail -1 | awk -F '"' '{print $2}')
 
-	echo "$i $x" | awk '{print 100*$1/$2 "%..."}'
+	pages=$((pages+1))
+
+	echo "$count $max $pages" | awk '{print "found=" $1, "looking_for=" $2, "pages=" $3, "progress=" 100*$1/$2 "%"}'
 done
 
 echo '</center>' >> index.html
