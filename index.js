2c0e54e2fe86e0c23967b2adb79f063c0279815e
assafmo <assaf.morami@gmail.com>
2017-01-31 08:47:15 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-/*var currentVersion = document.getElementById('version').getAttribute('content');
-function load() {
-
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://raw.githubusercontent.com/assafmo/nba-espn-stats-and-info/master/index.html');
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState !== XMLHttpRequest.DONE || xhr.status !== 200)
-			return;
-
-		var parser = new DOMParser();
-		var doc = parser.parseFromString(xhr.responseText, 'text/html');
-		var newVersion = doc.getElementById('version').getAttribute('content');
-
-		if (newVersion !== currentVersion) {
-			twttr.widgets.load(doc.getElementById('content'));
-
-			console.log('new');
-			currentVersion = newVersion;
-			document.getElementById('content').remove();
-
-			var domToInsert = doc.getElementById('content');
-			domToInsert.style.display = 'block';
-			var domToInsertAfter = document.getElementById('loader');
-			domToInsertAfter.parentNode.insertBefore(domToInsert, domToInsertAfter.nextSibling);
-		} else {
-			console.log('same');
-		}
-	}
-	xhr.send();
-}*/
 
 window.onload = function () {
 	setTimeout(function () {
